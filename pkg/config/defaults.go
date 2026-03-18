c07f5c948f6a849d6af38d0e389434904ed74943
dev-miro26 <121471669+dev-miro26@users.noreply.github.com>
2026-03-18 19:03:24 +0900

    refactor: centralize environment variable key constants (#1730)
    * refactor: centralize environment variable key constants
    
    * refactor: update environment variable constants and usage in gateway

--- pkg/config/defaults.go
+++ pkg/config/defaults.go
@@ -15,7 +15,7 @@ func DefaultConfig() *Config {
 	// Determine the base path for the workspace.
 	// Priority: $PICOCLAW_HOME > ~/.picoclaw
 	var homePath string
-	if picoclawHome := os.Getenv("PICOCLAW_HOME"); picoclawHome != "" {
+	if picoclawHome := os.Getenv(EnvHome); picoclawHome != "" {
 		homePath = picoclawHome
 	} else {
 		userHome, _ := os.UserHomeDir()
